5f66392d0072aa8cbce25b596c70936fe8f7bf0d
Avinash <avinashkanchanssn@gmail.com>
2022-06-22 18:53:28 +0530

    commit2

--- src/stringPrograms/DateTime.java
+++ src/stringPrograms/DateTime.java
@@ -1,7 +1,11 @@
 package stringPrograms;
 
+import java.util.Date;
+
 public class DateTime {
 public static void main(String[] args) {
 	System.out.println("heello");
+	Date date = new Date();
+	System.out.println(date.toString());
 }
 }
